fc912762d48146b62109c8a51fe3dcd30804dff8
paulgibes <paulgibes@gmail.com>
2023-03-03 15:59:48 -0700

    made template for markdown, evaluate the licenses, included package import statements

---
+++ utils/generateMarkdown.js
@@ -0,0 +1,70 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicense(license) {
+  switch (license) {
+    case "MIT":
+      // dont need a break if there is a return
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+    case "ISC":
+      return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)";
+    case "Perl":
+      return "[![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)";
+    default:
+      return "";
+  }
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  // return `# ${data.title}`;
+  return `# ${data.title}
+  
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#Usage)
+  ${data.license !== "None" ? "- [License](#license)" : ""}
+  - [Contributing](#Contributing)
+  - [Tests](#Tests)
+  - [Questions](#Questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.Usage}
+
+${data.license !== "None" ? "## License" : ""}
+${renderLicense(data.license)}
+
+## Contributing
+${data.Contributing}
+
+## Tests
+${data.Tests}
+
+## Questions
+${data.Questions}
+
+`;
+}
+
+module.exports = generateMarkdown;
+
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
